db5c1f5d14a27bfcadfaf5c2e2652c52ab4da05f
Eri365 <101698682+Eri365@users.noreply.github.com>
2023-07-02 21:21:25 +0800

    修復bug
    解決選擇第一個subject時，會無法成功submit的問題

--- CourseCrawler/main.py
+++ CourseCrawler/main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
 import time, json
 
 try:
@@ -8,30 +9,28 @@ try:
 except:
     print("ERROR:", __file__, "json 加載失敗")
 
+def query_table_data(driver: webdriver.Chrome, year_value: str, subject_value: str):
+    print(f'正在加載 {year_value} 學年期、{subject_value} 領域')
+    # 選擇 學年期
+    Select(driver.find_element('xpath', path['year_xpath'])).select_by_value(year_value)
+    # 選擇 通識分類
+    Select(driver.find_element('xpath', path['subject_xpath'])).select_by_value(subject_value)
+    # 找到 開始查詢 按鈕
+    
+    buttons = driver.find_elements('xpath', "//input[@type='submit']")
+    for button in buttons:
+        button.click()
+    return None
+
+subject_values = ['E', 'G', 'K', 'M', 'L', '3', '8', 'N']
+
 if __name__ == '__main__':
     driver = webdriver.Chrome()
     driver.get(path['url'])
 
-    select_year = Select(driver.find_element('xpath', path['year_xpath']))
-    select_year.select_by_value('1111')
-    
-    subject_option_count = len(Select(driver.find_element('xpath', path['subject_xpath'])).options)
-    for i in range(subject_option_count):
-        # 找到通識分類元素
-        select_subject = Select(driver.find_element('xpath', path['subject_xpath']))
-        # 取得所有選項
-        options = select_subject.options
-        # 取得當前索引值的 value
-        value = options[i].get_attribute("value")
-        print(f'正在加載 {value} 領域')  
-        # Select
-        select_subject.select_by_value(value)
-
-        # 找到 開始查詢 按鈕
-        button = driver.find_element('xpath', path['submit_xpath'])
-        # 開始查詢
-        button.click()
-        
+    for value in subject_values:
+        query_table_data(driver, '1111', value)
         time.sleep(1)
-        
     driver.quit()
+    
+# 
